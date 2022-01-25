ca3dd027f62d22f46049d9f1977bd7ab8b99f21f
sima-zhu <sizhu@microsoft.com>
2022-01-25 09:57:58 -0800

    Non stop on errors.

--- eng/common/scripts/Update-DocsMsMetadata.ps1
+++ eng/common/scripts/Update-DocsMsMetadata.ps1
@@ -196,16 +196,20 @@ function UpdateDocsMsMetadataForPackage($packageInfoJsonLocation, $packageInfo) 
 
 foreach ($packageInfoLocation in $PackageInfoJsonLocations) {
   Write-Host "Updating metadata for package: $packageInfoLocation"
-
-  # Convert package metadata json file to metadata json property.
-  $packageInfo = GetPackageInfoJson $packageInfoLocation
-  # Add validation step for daily update and release
-  if ($ValidateDocsMsPackagesFn -and (Test-Path "Function:$ValidateDocsMsPackagesFn")) {
-    &$ValidateDocsMsPackagesFn -PackageInfo $packageInfo -PackageSourceOverride $PackageSourceOverride -DocValidationImageId $DocValidationImageId -DocRepoLocation $DocRepoLocation
-    if ($LASTEXITCODE) {
-      LogError "The package failed Doc.Ms validation. Check https://aka.ms/azsdk/docs/docker for more details on how to diagnose this issue."
-      exit $LASTEXITCODE
+  try {
+    # Convert package metadata json file to metadata json property.
+    $packageInfo = GetPackageInfoJson $packageInfoLocation
+    # Add validation step for daily update and release
+    if ($ValidateDocsMsPackagesFn -and (Test-Path "Function:$ValidateDocsMsPackagesFn")) {
+      &$ValidateDocsMsPackagesFn -PackageInfo $packageInfo -PackageSourceOverride $PackageSourceOverride -DocValidationImageId $DocValidationImageId -DocRepoLocation $DocRepoLocation
+      if ($LASTEXITCODE) {
+        LogError "The package failed Doc.Ms validation. Check https://aka.ms/azsdk/docs/docker for more details on how to diagnose this issue."
+        exit $LASTEXITCODE
+      }
     }
+    UpdateDocsMsMetadataForPackage $packageInfoLocation $packageInfo
   }
-  UpdateDocsMsMetadataForPackage $packageInfoLocation $packageInfo
+  catch {
+    Write-Error $_
+  }
 }
